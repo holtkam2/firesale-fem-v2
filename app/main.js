d6220bdaaa5f5a9879e497ed271c1129a82ea0b4
jason <jholtkamp@rallyhealth.com>
2020-02-08 16:35:07 -0600

    now loads forzatracker in window (FT must be running)

--- app/main.js
+++ app/main.js
@@ -1,13 +1,23 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 let mainWindow = null;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ show: false });
-
-  mainWindow.loadFile(`${__dirname}/index.html`)
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    minWidth: 480,
+    icon: path.join(__dirname, 'assets/icons/mac/icons.icns')
   })
+
+  // and load the index.html of the app.
+  // mainWindow.loadURL(url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }))
+
+  // and load the url of the app.
+  mainWindow.loadURL('http://localhost:3000/#/')
 })
